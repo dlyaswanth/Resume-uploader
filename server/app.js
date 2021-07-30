9724c7f3df633f0927f71de341e3db1f63189106
Yaswanth L <65965757+dlyaswanth@users.noreply.github.com>
2021-07-30 18:53:15 +0530

    Add files via upload

--- server/app.js
+++ server/app.js
@@ -1,8 +1,17 @@
 const fastify=require('fastify')();
+const express=require('fastify-express')
 const PORT= process.env.PORT||5001;
 fastify.register(require('./routes/router'))
+fastify.register(express)
 const start= async ()=>
 {
+    if(process.env.NODE_ENV=="production"){
+        fastify.use(express.static('client/build'))
+            const path=require('path')
+            fastify.get("*",(req,res)=>{
+                res.sendFile(path.resolve(__dirname,'client','build','index.html'))
+            })
+            }
     try 
     {
         await fastify.listen(PORT);
